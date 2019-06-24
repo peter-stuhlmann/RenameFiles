ae13c6da69cb6564e60368f5eefc637e9d368a99
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-06-24 15:30:31 +0200

    Add color variables

--- rename.sh
+++ rename.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# colors
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+COLORRESET='\033[0m'
+
 TITLE=$2
 file_extension=()
 
 if [[ $# -lt 2 ]]; then
-  printf "Syntax error! Please enter a file extension.\n"
+  printf "${RED}Syntax error! ${COLORRESET}Please enter a file extension.\n"
 else
   for i in $@; do
     case $i in
