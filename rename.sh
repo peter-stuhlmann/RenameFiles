2dfd6634db011f27e56df9ffce800ee82c45a387
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-06-24 15:25:35 +0200

    Add a title variable
    The title must be entered as a second argument;
    It does not replace the file name, but is added as a prefix

--- rename.sh
+++ rename.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+TITLE=$2
 file_extension=()
 
-if [[ $# -lt 1 ]]; then
+if [[ $# -lt 2 ]]; then
   printf "Syntax error! Please enter a file extension.\n"
 else
   for i in $@; do
@@ -13,7 +14,7 @@ else
   done
 
   for i in *.${file_extension[@]}
-    do mv "$i" "TEST-${i/.${file_extension[@]}}".${file_extension[@]}
+    do mv "$i" "${TITLE}-${i/.${file_extension[@]}}".${file_extension[@]}
   done
 
 fi
